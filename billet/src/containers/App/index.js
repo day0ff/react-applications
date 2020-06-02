cfdc664a0931f11f1fa20f40d1e555d6b4e885fb
day0ff <day85@tut.by>
2020-06-02 20:29:21 +0300

    Add redux persistor.

--- billet/src/containers/App/index.js
+++ billet/src/containers/App/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from '../../store/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from '../../store/store';
 import Router from '../Router';
 
 function App() {
     return (
         <Provider store={store}>
-            <Router/>
+            <PersistGate  loading={null} persistor={persistor}>
+                <Router/>
+            </PersistGate>
         </Provider>
     );
 }
--- billet/src/store/store.js
+++ billet/src/store/store.js
@@ -1,8 +1,19 @@
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 import stationsReducer from './reducers/stations-reducer';
 
 const rootReducer = combineReducers({
     stations:stationsReducer
 });
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export const persistor = persistStore(store);
